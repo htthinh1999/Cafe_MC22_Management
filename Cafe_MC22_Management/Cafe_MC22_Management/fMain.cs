8d52f25678471e9ea1cdb6f12513641e47d2606b
htthinh1999 <htthinh1999@gmail.com>
2021-03-04 14:53:05 +0700

    Create solution structure

---
+++ Cafe_MC22_Management/Cafe_MC22_Management/fMain.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cafe_MC22_Management
+{
+    public partial class fMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
